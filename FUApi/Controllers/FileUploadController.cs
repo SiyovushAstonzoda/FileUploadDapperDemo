2ecdb9e571320c2afeec3acc668ebf8297b354f9
Siyovush Astonzoda <astonov77@gmail.com>
2023-10-02 19:56:57 +0500

    Added file upload controller, wwwroot folder, and UseStaticFiles method

---
+++ FUApi/Controllers/FileUploadController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FUApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FileUploadController : ControllerBase 
+{
+    public FileUploadController()
+    {
+        
+    }
+
+    [HttpPost("Upload File")]
+    public string UploadFile(IFormFile file)
+    {
+        return "Ok";
+    }
+}
