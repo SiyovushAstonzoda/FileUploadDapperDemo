38aaa59036d0310cd7fd5479f8b41754394ffa08
Siyovush Astonzoda <astonov77@gmail.com>
2023-10-03 11:38:11 +0500

    Sixth Commit
    Added Get list of Files using Path.Combine()

--- FUApi/Controllers/FileUploadController.cs
+++ FUApi/Controllers/FileUploadController.cs
@@ -18,10 +18,21 @@ public class FileUploadController : ControllerBase
     {
         var currentFolder = _webHostEnvironment.WebRootPath;
         var fullPath = Path.Combine(currentFolder, "images", file.FileName);
+        //var exists = System.IO.File.Exists(fullPath); 
         using (var stream = new FileStream(fullPath, FileMode.Create))
         {
-             file.CopyTo(stream);
+            file.CopyTo(stream);
         }
             return fullPath;
+    }
+
+    [HttpGet("Get List of Files")]
+    public IEnumerable<string> GetListOfFiles()
+    {
+        var path = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+
+        var listOfFiles = Directory.GetFiles(path);
+
+        return listOfFiles;
     }
 }
